4e7ca24d6207903647cad5fdca7aff4a0693ed2d
Joseph Barbosa <jbarbosa@microsoft.com>
2025-01-22 11:53:18 -0800

    initial commit to include WebGoatCore

---
+++ WebGoatCore/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using WebGoatCore.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebGoatCore.ViewModels;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
+
+namespace WebGoatCore.Controllers
+{
+    [AllowAnonymous]
+    public class HomeController : Controller
+    {
+        private readonly ProductRepository _productRepository;
+        private IConfiguration _configuration;
+
+        public HomeController(ProductRepository productRepository, IConfiguration configuration)
+        {
+            _productRepository = productRepository;
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            string clientSecret = _configuration["Secrets:EntraID:ClientSecret"];
+            return View(new HomeViewModel()
+            {
+                TopProducts = _productRepository.GetTopProducts(4),
+                ClientSecret = clientSecret
+            });
+        }
+
+        public IActionResult About() => View();
+
+        [Authorize(Roles = "Admin")]
+        public IActionResult Admin() => View();
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel
+            {
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier,
+                ExceptionInfo = HttpContext.Features.Get<IExceptionHandlerPathFeature>(),
+            });
+        }
+    }
+}
